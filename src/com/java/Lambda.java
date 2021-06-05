01eecc92abe685c0086601779328e230b18f31b3
LENOVO <LENOVO@DESKTOP-G1OD6KK>
2021-06-05 17:35:15 +0530

    person 1 lambda

--- src/com/java/Lambda.java
+++ src/com/java/Lambda.java
@@ -1,12 +1,14 @@
 package com.java;
 interface Drawing{
-	public void draw();
+	public void draw(int a);
 	
 }
 
 public class Lambda {
 public static void main(String[] args) {
-	Drawing a=()->System.out.println("hellllllo");
-	a.draw();
+	/*Drawing a=()->System.out.println("hellllllo");
+	a.draw();*/
+	Drawing d1=(a)->System.out.println(a);
+	d1.draw(10);
 }
 }
